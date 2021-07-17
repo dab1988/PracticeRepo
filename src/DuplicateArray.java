f7d15f0873e5fcc54661d30375b968fe943fca0a
Dabeer Haider <Dabeer.Haider@telusinternational.com>
2021-07-17 17:42:40 +0530

    new code

--- src/DuplicateArray.java
+++ src/DuplicateArray.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -12,6 +13,24 @@ public class DuplicateArray {
 	{
 		// TODO Auto-generated method stub
 
+		
+		/*
+		 * 
+		 * 
+		 
+		 for(int i=0; i<p.length; i++)
+		{
+		
+			if(p[i]<0)
+			{			
+					
+			list.add(p[i]);
+				
+			}	
+		}
+		 
+		 * 
+		 */
 		
 		int a []= {2,3,3,4,5,5,6,7,8,8};
 		
@@ -27,10 +46,12 @@ public class DuplicateArray {
 			}
 		}
 		 
-		System.out.println(set +"-->"+ count);
+	//	System.out.println(set +"-->"+ count);
 		
 		
-		int k[]= {8,7,-2,-5,0,4,3,-9};
+		//int k[]= {1, -1, 3, 2, -7, -5, 11, 6};
+		
+		int p[]= {8,7,-2,-5,0,4,3,-9};
 		
 		//List<Integer> list =Arrays.stream(k).boxed().collect(Collectors.toList());
 		
@@ -38,12 +59,22 @@ public class DuplicateArray {
 		
 		ArrayList<Integer> list= new ArrayList<Integer>();
 		
-		for(int i=0; i<k.length; i++)
+		for(int i=0; i<p.length; i++)
 		{
 		
-			if(k[i]<0)
+			if(p[i]>=0)
 			{
-			list.add(k[i]);
+			list.add(p[i]);
+			Collections.sort(list);
+			}
+			
+			else if (p[i]<0)
+			{
+							
+						
+				list.add(p[i]);
+					
+				//Collections.sort(list);
 			}
 			
 		}
